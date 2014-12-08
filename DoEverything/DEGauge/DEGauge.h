174802c355bb97963aec2b54d147bb57505fe225
skswkd5 <skswkd5@solutus.co.kr>
2014-12-08 22:25:18 +0900

    Guage 추가하

---
+++ DoEverything/DEGauge/DEGauge.h
@@ -0,0 +1,13 @@
+//
+//  DEGauge.h
+//  DoEverything
+//
+//  Created by Jeesun Kim on 2014. 12. 5..
+//  Copyright (c) 2014년 skswkd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEGauge : UIView
+
+@end
---
+++ DoEverything/DEGauge/DEGauge.m
@@ -0,0 +1,21 @@
+//
+//  DEGauge.m
+//  DoEverything
+//
+//  Created by Jeesun Kim on 2014. 12. 5..
+//  Copyright (c) 2014년 skswkd. All rights reserved.
+//
+
+#import "DEGauge.h"
+
+@implementation DEGauge
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
